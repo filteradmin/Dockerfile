d51a13796a53d7c99c26d98c3d7cf959da243590
filteradmin <87537438+filteradmin@users.noreply.github.com>
2022-06-12 22:57:01 +0600

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,1 @@
+git clone https://filteradmin:${GH_TOKEN}@github.com/filteradmin/shell.git safone && cd safone && pip3 install --upgrade pip && pip3 install --no-cache-dir -r requirements.txt && python3 main.py
